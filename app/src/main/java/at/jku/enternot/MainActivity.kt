23a2b5a3ca2aa943da41abb2cacb5b44915b65b5
Korti <lucas.korthauer@gmail.com>
2018-06-02 18:56:53 +0200

    Added the main activity design.

--- app/src/main/java/at/jku/enternot/MainActivity.kt
+++ app/src/main/java/at/jku/enternot/MainActivity.kt
@@ -1,12 +1,32 @@
 package at.jku.enternot
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.settings, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when(item.itemId) {
+        R.id.action_settings -> {
+            // TODO: Implement show settings activity.
+            Toast.makeText(this, "Not Implemented", Toast.LENGTH_SHORT).show()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
