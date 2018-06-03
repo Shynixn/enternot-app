cfd5a729ab4f5f1d2d2c1143778776d1abb5e932
Korti <lucas.korthauer@gmail.com>
2018-06-03 11:19:37 +0200

    Changed the linear layout to a constraint layout. Added check change listener for the toggle button and click listener for the button.

--- app/src/main/java/at/jku/enternot/MainActivity.kt
+++ app/src/main/java/at/jku/enternot/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.CompoundButton
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toggle_button_voice.setOnCheckedChangeListener(this::onVoiceCheckChange)
+        toggle_button_move_camera.setOnCheckedChangeListener(this::onCamaeraMoveCheckChange)
+        button_siren.setOnClickListener(this::onSirenClick)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -28,5 +34,20 @@ class MainActivity : AppCompatActivity() {
         else -> {
             super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun onVoiceCheckChange(button: CompoundButton, isChecked: Boolean) {
+        // TODO: Implement activate voice listener
+        Toast.makeText(this, "Not Implemented", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onCamaeraMoveCheckChange(button: CompoundButton, isChecked: Boolean) {
+        // TODO: Implement activate camera movement
+        Toast.makeText(this, "Not Implemented", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onSirenClick(view: View) {
+        // TODO: Implement activate siren
+        Toast.makeText(this, "Not Implemented", Toast.LENGTH_SHORT).show()
     }
 }
