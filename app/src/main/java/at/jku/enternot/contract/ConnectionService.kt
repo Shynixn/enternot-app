6b1f52ef883867501f7352bca611a574ed27b05b
Christoph B <c.burghuber@indi.systems>
2018-06-13 17:54:39 +0200

    Fixed connection.

--- app/src/main/java/at/jku/enternot/contract/ConnectionService.kt
+++ app/src/main/java/at/jku/enternot/contract/ConnectionService.kt
@@ -1,5 +1,6 @@
 package at.jku.enternot.contract
 
+import android.content.Context
 import java.io.IOException
 
 interface ConnectionService {
@@ -9,5 +10,13 @@ interface ConnectionService {
      * @throws [IOException] when the request to the server fails.
      */
     @Throws(IOException::class)
-    fun <T> post(url: String, item: T? = null): Int
+    fun <T> post(url: String, context: Context, item: T? = null): Int
+
+    /**
+     * Sends a get request to the given relative [url] with the optional url [parameters].
+     * Returns the response content as object of class T. When no content is requested [clazz] should be
+     * [Int] to return the status code instead.
+     */
+    @Throws(IOException::class)
+    fun <T> get(url: String, clazz: Class<T>, context: Context, parameters: Map<String, String>?): T
 }
--- app/src/main/java/at/jku/enternot/service/ConnectionServiceIml.kt
+++ app/src/main/java/at/jku/enternot/service/ConnectionServiceIml.kt
@@ -1,37 +1,74 @@
 package at.jku.enternot.service
 
+import android.content.Context
 import at.jku.enternot.contract.ConfigurationService
 import at.jku.enternot.contract.ConnectionService
-import com.android.volley.Request
-import com.android.volley.Response
-import java.io.IOException
-import com.android.volley.VolleyError
-import org.json.JSONObject
-import com.android.volley.toolbox.JsonObjectRequest
 import com.google.gson.Gson
-import com.google.gson.JsonObject
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.Charset
 
-
-class ConnectionServiceIml(private val configurationService : ConfigurationService) : ConnectionService {
+class ConnectionServiceIml(private val configurationService: ConfigurationService) : ConnectionService {
+    private val connectionTimeOut = 5000
 
     /**
      * Sends a post request to the given relative [url] with the optional [item] as payload.
+     * Returns the status code of the http request.
      * @throws [IOException] when the request to the server fails.
      */
-    @Throws(IOException::class)
-    override fun <T> post(url: String, item: T?): Int {
-        val url = "http://my-json-feed"
+    override fun <T> post(url: String, context: Context, item: T?): Int {
+        // As we only use a very small communication layer we do not need big data processing like Volley would
+        // provide. A basic connection is enough
+        val configuration = configurationService.getConfiguration(context)
+        if (configuration == null) {
+            throw IllegalArgumentException("Configuration cannot be null!")
+        }
 
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.POST, url,JSONObject(Gson().toJson(item)),
-                Response.Listener { response ->
-                    textView.text = "Response: %s".format(response.toString())
-                },
-                Response.ErrorListener { error ->
-                    // TODO: Handle error
-                }
-        )
+        val url = URL(configuration.hostname + url)
+        val conn = url.openConnection() as HttpURLConnection
 
-// Access the RequestQueue through your singleton class.
-        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+        conn.setConnectTimeout(connectionTimeOut)
+        conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8")
+        conn.setRequestMethod("POST")
+        conn.outputStream.use { outputStream ->
+            if (item != null) {
+                outputStream.write(Gson().toJson(item)!!.toByteArray(Charsets.UTF_8))
+            }
+        }
+        conn.disconnect()
+
+        return conn.responseCode
+    }
+
+    /**
+     * Sends a get request to the given relative [url] with the optional url [parameters].
+     * Returns the response content as object of class T. When no content is requested [clazz] should be
+     * [Int] to return the status code instead.
+     */
+    override fun <T> get(url: String, clazz : Class<T>, context: Context, parameters: Map<String, String>?): T {
+        // As we only use a very small communication layer we do not need big data processing like Volley would
+        // provide. A basic connection is enough
+        val configuration = configurationService.getConfiguration(context)
+        if (configuration == null) {
+            throw IllegalArgumentException("Configuration cannot be null!")
+        }
+
+        var responseItem : Any?
+        val url = URL(configuration.hostname + url)
+        val conn = url.openConnection() as HttpURLConnection
+
+        conn.setConnectTimeout(connectionTimeOut)
+        conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8")
+        conn.setRequestMethod("GET")
+        responseItem = conn.responseCode
+
+        conn.inputStream.use { inputStream ->
+            val reader = inputStream.bufferedReader(Charsets.UTF_8)
+            responseItem = Gson().fromJson(reader,clazz)
+        }
+        conn.disconnect()
+
+        return responseItem as T
     }
 }
